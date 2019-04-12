3bccfb247f2e252f5d792aef7aeb81a40c621bbd
robertreid15 <35927912+robertreid15@users.noreply.github.com>
2019-04-12 16:47:34 +0100

    Added test files

---
+++ UnitTesting/UnitTesting/MyMathTest.java
@@ -0,0 +1,13 @@
+package UnitTesting;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		MyMath math = new MyMath(8, 10);
+	    assertEquals(18, math.practice());	}
+
+}
