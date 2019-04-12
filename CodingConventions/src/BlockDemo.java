824954c157ee3d768609f9f94ec38da9b72b6ca9
robertreid15 <35927912+robertreid15@users.noreply.github.com>
2019-04-12 18:36:14 +0100

    Add files via upload
    Added Block Demo

---
+++ CodingConventions/src/BlockDemo.java
@@ -0,0 +1,16 @@
+/**
+ * This class illustrates the use of blocks i.e. a group of zero 
+ * or more statements between balanced braces. 
+ * Also shows an example of correctly formatting an if else statement
+ */
+class BlockDemo {
+     public static void main(String[] args) {
+          boolean condition = true;
+          if (condition) { // begin block 1
+               System.out.println("Condition is true.");
+          } // end block one
+          else { // begin block 2
+               System.out.println("Condition is false.");
+          } // end block 2
+     }
+}
